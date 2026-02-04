b89a165f7854b117e8b9b8b43349d7e9ec3f7d93
Jean-baptiste <dylanj.jean-baptiste@grenoble-inp.org>
2026-02-04 17:25:42 +0100

    feat: Resolve problem 31

---
+++ problems/problem_031.c
@@ -0,0 +1,51 @@
+/**
+* Problem 31: Coin sums
+ *
+ * Goal: How many different ways can £2 be made using any number of coins?
+ *
+ * Difficulty: 5%
+ */
+
+#include <stdio.h>
+#include "../utils/timer.h"
+
+
+/**
+ * @brief Counts the number of ways to make a sum with coins.
+ * Uses dynamic programming to solve the "coin change" problem.
+ *
+ * Complexity: O(n * m) where n = target, m = number of coins
+ *
+ * @return The number of different ways to make the target sum
+ */
+long long solve(const int target) {
+
+    const int coins[8] = {1,2,5,10,20,50,100,200};
+    long long result[target + 1];
+
+    result[0] = 1;
+
+    for (int i = 1; i <= target; i++) {
+        result[i] = 0;
+    }
+
+    for (int c = 0; c < 8; c++) {
+        for (int s = coins[c]; s <= target; s++) {
+            result[s] += result[s - coins[c]];
+        }
+    }
+
+    return result[target];
+
+}
+
+/**
+ * @brief Main function
+ */
+int main() {
+
+    start_timer();
+    const long long result = solve(200);
+    printf(" result = %lld\n", result);
+    stop_timer();
+}
